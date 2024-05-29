28827f0c9adcdf76f0895df8d72f22de31d64f66
Ventiler <pablolurquilopez@users.noreply.github.com>
2024-05-29 14:26:02 +0200

    Update main.tf
    To avoid sissiues with AWS provider.

--- main.tf
+++ main.tf
@@ -22,3 +22,12 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+}
